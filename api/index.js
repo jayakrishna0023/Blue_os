9acfce9b4bcbe3e4e932d8e04b59ff87d7af7daa
Jaya Krishna <jayakrishna0023@gmail.com>
2025-12-20 19:27:33 +0530

    Fix Vercel API routing - properly handle /api prefix in serverless function

--- api/index.js
+++ api/index.js
@@ -1,3 +1,12 @@
+// Vercel Serverless Function Entry Point
 const app = require('../server/index');
 
-module.exports = app;
+// Export as a serverless function handler
+module.exports = (req, res) => {
+    // Vercel strips the /api prefix, but Express routes expect it
+    // So we need to add it back if missing
+    if (!req.url.startsWith('/api')) {
+        req.url = '/api' + req.url;
+    }
+    return app(req, res);
+};
